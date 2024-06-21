a7b8058263cc42bae9e2f2acdb19cd2e475d3f41
HensalRai <hensalrai@yahoo.com>
2024-06-21 13:20:21 +0545

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import "./screen/mainUI.dart";
 
-void main() {
+void main() async{
+   await Supabase.initialize(
+    url: 'https://lqevkoivmmqwzumdfgef.supabase.co',
+    anonKey:
+         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxZXZrb2l2bW1xd3p1bWRmZ2VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg4NjExNDMsImV4cCI6MjAzNDQzNzE0M30.QYnGKTBuyUjFFGGcJmCH-CABN4mcKndOc2hzw12SU_M',
+  );
   runApp(const MaterialApp(
     home: MyApp(),
     debugShowCheckedModeBanner: false,
--- lib/repository/supa.dart
+++ lib/repository/supa.dart
@@ -2,25 +2,28 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import '../model/cat.dart';
 
 class Supa {
-  late final client;
-  Supa() {
-    client = SupabaseClient('https://lqevkoivmmqwzumdfgef.supabase.co',
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxZXZrb2l2bW1xd3p1bWRmZ2VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg4NjExNDMsImV4cCI6MjAzNDQzNzE0M30.QYnGKTBuyUjFFGGcJmCH-CABN4mcKndOc2hzw12SU_M');
-  }
+  final _supbaseClient = Supabase.instance.client;
 
   Future insertData({required name, required breed}) async {
-    final res = await client.from('cat').insert({'name': name, 'Breed': breed});
+    final res =
+        await _supbaseClient.from('cat').insert({'name': name, 'Breed': breed});
   }
 
-  Future fetch() async {
-    final res = await client.from('cat').select('*');
+  // Future fetch() async {
+  //   final res = await client.from('cat').select('*');
 
-    if (res.data == null) {
-      print('No data found');
-      return [];
-    } else {
-      final data = res.data as List;
-      return data.map((json) => cat.fromJson(json)).toList();
-    }
+  //   if (res.data == null) {
+  //     print('No data found');
+  //     return [];
+  //   } else {
+  //     final data = res.data as List;
+  //     return data.map((json) => cat.fromJson(json)).toList();
+  //   }
+  // }
+
+  fetch() async {
+    final res = await _supbaseClient.from('cat').select('*');
+    final data = res as List;
+    return data.map((json) => cat.fromJson(json)).toList();
   }
 }
